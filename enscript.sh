0585ad212f6575ac1ac1457e30835e16d2e7c97b
branflakes2 <brianw5@umbc.edu>
2017-12-12 01:38:52 -0500

    ps files made

---
+++ enscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in *.vhd; do
+    [ -f "$i" ] || break
+    enscript --columns=2 --line-numbers --fancy-header --borders --landscape --pretty-print=vhdl --output "$i.ps" --header "$i" $i
+done
