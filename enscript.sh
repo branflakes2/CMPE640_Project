d25362cd44b2eb689db7710f1979d93d6b8ed1ba
branflakes2 <brianw5@umbc.edu>
2017-12-12 21:45:45 -0500

    lvs

--- enscript.sh
+++ enscript.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-for i in *.vhd; do
-    [ -f "$i" ] || break
-    enscript --columns=2 --line-numbers --fancy-header --borders --landscape --pretty-print=vhdl --output "$i.ps" $i
-    ps2pdf $i.ps
-done
-
-for i in *.txt; do
+for i in *LVS*; do
     [ -f "$i" ] || break
     enscript --columns=2 --line-numbers --fancy-header --borders --landscape --pretty-print=vhdl --output "$i.ps" $i
     ps2pdf $i.ps
